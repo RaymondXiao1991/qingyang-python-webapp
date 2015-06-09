79dfe233089755bdf7c398108a6efe2ab54e40fe
unknown <RaymondXiao1991@hotmail.com>
2015-06-10 01:22:01 +0800

    add wsgiapp.py

---
+++ www/wsgiapp.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# coding: utf-8
+
+__author__ = 'Raymond Xiao'
+
+# 创建一个Web App的启动文件wsgiapp.py，负责初始化数据库、初始化Web框架，然后加载urls.py，最后启动Web服务：
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import os
+
+from transwarp import db
+from transwarp.web import WSGIApplication, Jinja2TemplateEngine
+
+from config import configs
+
+# 初始化数据库：
+db.create_engine(**configs.db)
+
+# 创建一个WSGIApplication:
+wsgi = WSGIApplication(os.path.dirname(os.path.abspath(__file__)))
+# 初始化jinja2模板引擎:
+template_engine = Jinja2TemplateEngine(os.path.dirname(os.path.abspath(__file__)), 'templates')
+wsgi.template_engine = template_engine
+
+# 加载带有@get/@post的URL处理函数:
+import urls
+wsgi.add_module(urls)
+
+# 在9000端口上启动本地测试服务器:
+if __name__ == '__main__':
+	wsgi.run(9000)
