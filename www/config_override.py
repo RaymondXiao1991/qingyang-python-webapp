fd794422326c1d58d95f998efaadf29d4d44699b
qingyang <RaymondXiao1991@hotmail.com>
2015-06-09 11:27:42 +0800

    add config_override.py

---
+++ www/config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding: utf-8
+
+'''
+Override configurations.
+生产环境的标准配置
+'''
+
+__author__ = 'Raymond Xiao'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
